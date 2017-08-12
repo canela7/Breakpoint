3d9428297fbfb2d89330ec1efac79d56fb07675e
canela7 <canelabrian123@gmail.com>
2017-08-11 19:09:41 -0700

    Creating GroupCell
    created Group Cell

---
+++ Breakpoint/View/GroupCell.swift
@@ -0,0 +1,28 @@
+//
+//  GroupCell.swift
+//  Breakpoint
+//
+//  Created by Brian Canela on 8/11/17.
+//  Copyright © 2017 CanelaApps. All rights reserved.
+//
+
+import UIKit
+
+class GroupCell: UITableViewCell {
+
+    @IBOutlet weak var groupTitleLbl: UILabel!
+    @IBOutlet weak var groupDescLbl: UILabel!
+    @IBOutlet weak var memberCountLbl: UILabel!
+    
+    func configureCell(title: String, description: String, memberCount: Int) {
+        self.groupTitleLbl.text = title
+        self.groupDescLbl.text = description
+        self.memberCountLbl.text = "\(memberCount) members."
+        
+        
+    }
+    
+    
+    
+}
+
